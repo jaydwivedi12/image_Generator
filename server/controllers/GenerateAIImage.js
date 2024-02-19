9663de3bd556ba234c0595f186467c66a12727e9
Vinodchouhan01 <vinodchouhangiri@gmail.com>
2024-02-20 02:02:58 +0530

    First commit

---
+++ server/controllers/GenerateAIImage.js
@@ -0,0 +1,33 @@
+import * as dotenv from "dotenv";
+import { createError } from "../error.js";
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+});
+dotenv.config();
+
+
+// Controller to generate Image
+
+export const generateImage = async (req, res, next) => {
+  try {
+    const { prompt } = req.body;
+
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+    const generatedImage = response.data.data[0].b64_json;
+    return res.status(200).json({ photo: generatedImage });
+  } catch (error) {
+    next(
+      createError(
+        error.status,
+        error?.response?.data?.error?.message || error?.message
+      )
+    );
+  }
+};
